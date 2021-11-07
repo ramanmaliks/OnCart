3a050aadac49508c09a108965eba5c462d86c9da
Raman Malik <ramanmaliks@gmail.com>
2021-11-07 22:45:25 +0530

    First Commit

---
+++ OnCartProject/OnCartWebParent/OnCartBackEnd/src/main/java/com/oncart/admin/OnCartBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.oncart.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnCartBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnCartBackEndApplication.class, args);
+	}
+
+}
---
+++ OnCartProject/OnCartWebParent/OnCartFrontEnd/src/main/java/com/oncart/site/OnCartFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.oncart.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnCartFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnCartFrontEndApplication.class, args);
+	}
+
+}
